837815bc37367f6f4b077edd482326e1119fcd29
Salim Benfarhat <tribuhut@protonmail.com>
2017-02-28 15:13:11 +0100

    adding seeds for tribe model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tribe1 = Tribe.create(name: 'French Tribe', about: 'French Tribe by french people')
+tribe2 = Tribe.create(name: 'English Tribe', about: 'English Tribe by english people')
+tribe3 = Tribe.create(name: 'Spanish Tribe', about: 'Spanish Tribe by spanish people')
+tribe4 = Tribe.create(name: 'Egyptian Tribe', about: 'Egyptian Tribe by egyptian people')
