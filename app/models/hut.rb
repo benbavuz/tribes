c621997d3846cb34edc796b516b042d2f16b28f3
Fabien Martinez <fab.mrtnz22@gmail.com>
2017-02-28 17:16:50 +0100

    add validates options in hut

--- app/models/hut.rb
+++ app/models/hut.rb
@@ -1,5 +1,11 @@
 class Hut < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :zip_code, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
+  validates :capacity, presence: true
 
   has_many :hut_equipments
   has_many :equipments, through: :hut_equipments
