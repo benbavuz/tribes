19ed2b387b070e1dc571a5493a270f68dd088808
Fabien Martinez <fab.mrtnz22@gmail.com>
2017-02-28 15:59:19 +0100

    model validates tribes -> user added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :tribe_members
+  has_many :tribes, through: :tribe_members
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
