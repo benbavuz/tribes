b62b7111c0529176d7c338aa7b66368c2bde7f6a
salim lalam <salim.lalam@gmail.com>
2017-02-28 12:34:46 +0100

    create equipment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228110329) do
+ActiveRecord::Schema.define(version: 20170228113336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "equipment", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "huts", force: :cascade do |t|
     t.string   "name"
