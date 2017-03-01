0eea94b5f07ac0a951538de3dd83e4dd6355ef9f
benjamin bavuz <benjamin.bavuz@gmail.com>
2017-03-01 11:15:47 +0100

    huts controller

--- app/controllers/huts_controller.rb
+++ app/controllers/huts_controller.rb
@@ -18,14 +18,22 @@ class HutsController < ApplicationController
     @hut.save!
   end
 
+  def edit
+    @hut = Hut.find(params[:id])
+  end
+
   def update
+    @hut.update(hut_params)
+  end
+
+  def destroy
+    @hut.destroy
   end
 
 private
 
-def hut_params
-
-params.require(:hut).permit(:name, :address1, :address2, :zip_code, :country, :capacity)
-end
+  def hut_params
+  params.require(:hut).permit(:name, :address1, :address2, :zip_code, :country, :capacity)
+  end
 
 end
