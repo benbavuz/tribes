6caefe652a240909d0c0ec6e0cf32b0e118ba1f9
sbenfarhat <s.benfarhat@my.com>
2017-03-08 11:15:25 +0100

    finish profil page

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :users, only: [] do
   get '/huts', to: "huts#index_by_user"
   get '/tribes', to: "tribes#index_by_user"
+  get '/profile', to: "pages#profile"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 
